c12dbf160d4a55fcaa0291c3e2f571e82a4ec1ee
Maryam <93725125+Maryousefi@users.noreply.github.com>
2025-08-20 06:17:19 +0330

    Update symbols_fa.py

--- text/symbols_fa.py
+++ text/symbols_fa.py
@@ -1,17 +1,9 @@
-
 _pad = "_"
 _eos = "~"
 
-# Persian alphabet (grapheme-based)
 _persian_letters = list("آابپتثجچحخدذرزسشصضطظعغفقکگلمنوهی")
-
-# Latin letters and digits
 _latin_letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
 _digits = list("0123456789")
+_punctuation = list("!\"'(),-.:;؟،؛ ")
 
-# Basic punctuation
-_punctuation = list("!\"'(),-.:;? ")
-
-# Combine all
 symbols = [_pad, _eos] + _persian_letters + _latin_letters + _digits + _punctuation
-
