5ac66aa9b2c698230548c67478c69670a6153bdd
Maryam <93725125+Maryousefi@users.noreply.github.com>
2025-08-20 06:18:07 +0330

    Update __init__.py

--- text/__init__.py
+++ text/__init__.py
@@ -1,54 +1,24 @@
-""" from https://github.com/keithito/tacotron """
 from text import cleaners
-from text.symbols import symbols
+from text import cleaners_fa
+from text.symbols import symbols as en_symbols
+from text.symbols_fa import symbols as fa_symbols
+
+# Map cleaner names to functions
+cleaner_names = {
+    "english_cleaners": cleaners.english_cleaners,
+    "english_cleaners2": cleaners.english_cleaners2,
+    "persian_cleaners": cleaners_fa.persian_cleaners,
+}
+
+# Default: English symbols
+symbols = en_symbols
 
 
-# Mappings from symbol to numeric ID and vice versa:
-_symbol_to_id = {s: i for i, s in enumerate(symbols)}
-_id_to_symbol = {i: s for i, s in enumerate(symbols)}
-
-
-def text_to_sequence(text, cleaner_names):
-  '''Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
-    Args:
-      text: string to convert to a sequence
-      cleaner_names: names of the cleaner functions to run the text through
-    Returns:
-      List of integers corresponding to the symbols in the text
-  '''
-  sequence = []
-
-  clean_text = _clean_text(text, cleaner_names)
-  for symbol in clean_text:
-    symbol_id = _symbol_to_id[symbol]
-    sequence += [symbol_id]
-  return sequence
-
-
-def cleaned_text_to_sequence(cleaned_text):
-  '''Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
-    Args:
-      text: string to convert to a sequence
-    Returns:
-      List of integers corresponding to the symbols in the text
-  '''
-  sequence = [_symbol_to_id[symbol] for symbol in cleaned_text]
-  return sequence
-
-
-def sequence_to_text(sequence):
-  '''Converts a sequence of IDs back to a string'''
-  result = ''
-  for symbol_id in sequence:
-    s = _id_to_symbol[symbol_id]
-    result += s
-  return result
-
-
-def _clean_text(text, cleaner_names):
-  for name in cleaner_names:
-    cleaner = getattr(cleaners, name)
-    if not cleaner:
-      raise Exception('Unknown cleaner: %s' % name)
-    text = cleaner(text)
-  return text
+def get_symbols(cleaner_name: str):
+    """Return symbol set depending on cleaner."""
+    global symbols
+    if cleaner_name == "persian_cleaners":
+        symbols = fa_symbols
+    else:
+        symbols = en_symbols
+    return symbols
