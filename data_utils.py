4b414e9303b20a815a57f3e10899686c6f47b855
Maryam <93725125+Maryousefi@users.noreply.github.com>
2025-10-06 01:41:50 +0330

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -2,26 +2,25 @@ import os
 import random
 import torch
 import numpy as np
-from torch.utils.data import Dataset
 import librosa
-
+from torch.utils.data import Dataset
+import commons
 from mel_processing import spectrogram_torch
-from text import text_to_sequence
 
 
 class TextAudioLoader(Dataset):
     """
     Loads text and audio pairs from filelists.
-    Each line in filelist: wav_path|text
+    Each line of filelist: wav_path|text
     """
 
     def __init__(self, filelist_path, data_config):
         self.audiopaths_and_text = self._load_filelist(filelist_path)
-        self.text_cleaners = data_config.text_cleaners
         self.sampling_rate = data_config.sampling_rate
         self.filter_length = data_config.filter_length
         self.hop_length = data_config.hop_length
         self.win_length = data_config.win_length
+        self.text_cleaners = data_config.text_cleaners
 
     def _load_filelist(self, filelist_path):
         with open(filelist_path, "r", encoding="utf-8") as f:
@@ -36,26 +35,29 @@ class TextAudioLoader(Dataset):
 
     def get_audio_text_pair(self, audiopath_and_text):
         audiopath, text = audiopath_and_text
-
-        # Load audio
-        audio, sr = librosa.load(audiopath, sr=self.sampling_rate)
-        audio = torch.FloatTensor(audio)
-
-        # Compute spectrogram (513 channels for 1024 FFT)
-        spec = spectrogram_torch(
-            audio.unsqueeze(0),
-            n_fft=self.filter_length,
-            sampling_rate=self.sampling_rate,
-            hop_size=self.hop_length,
-            win_size=self.win_length,
-            center=False,
-        ).squeeze(0)
-
-        # Process text
+        audio = self.get_audio(audiopath)
+        spec = self.get_spec(audio)
+        from text import text_to_sequence
         text_seq = text_to_sequence(text, self.text_cleaners)
-        text_tensor = torch.LongTensor(text_seq)
+        return torch.LongTensor(text_seq), spec, torch.FloatTensor(audio)
 
-        return text_tensor, spec, audio
+    def get_audio(self, filename):
+        audio, sr = librosa.load(filename, sr=self.sampling_rate)
+        audio = torch.FloatTensor(audio)
+        return audio
+
+    def get_spec(self, audio):
+        audio = audio.unsqueeze(0)
+        spec = spectrogram_torch(
+            audio,
+            self.filter_length,
+            self.sampling_rate,
+            self.hop_length,
+            self.win_length,
+            center=False,
+        )
+        spec = torch.squeeze(spec, 0)
+        return spec
 
     def __getitem__(self, index):
         return self.get_audio_text_pair(self.audiopaths_and_text[index])
@@ -70,44 +72,29 @@ class TextAudioCollate:
     """
 
     def __call__(self, batch):
-        # Sort by spectrogram length (descending)
-        batch.sort(key=lambda x: x[1].shape[1], reverse=True)
+        batch.sort(key=lambda x: x[1].size(1), reverse=True)
 
-        # Text
         input_lengths = torch.LongTensor([len(x[0]) for x in batch])
         max_input_len = input_lengths.max().item()
         text_padded = torch.zeros(len(batch), max_input_len, dtype=torch.long)
         for i in range(len(batch)):
             text_padded[i, :input_lengths[i]] = batch[i][0]
 
-        # Spectrogram
-        spec_lengths = torch.LongTensor([x[1].shape[1] for x in batch])
-        n_mel_channels = batch[0][1].shape[0]
+        spec_lengths = torch.LongTensor([x[1].size(1) for x in batch])
         max_spec_len = spec_lengths.max().item()
-        spec_padded = torch.zeros(len(batch), n_mel_channels, max_spec_len)
+        spec_padded = torch.zeros(len(batch), batch[0][1].size(0), max_spec_len)
         for i in range(len(batch)):
             spec_padded[i, :, :spec_lengths[i]] = batch[i][1]
 
-        # Audio
-        wav_lengths = torch.LongTensor([x[2].size(0) for x in batch])
-        max_wav_len = wav_lengths.max().item()
-        wav_padded = torch.zeros(len(batch), max_wav_len)
+        y_lengths = torch.LongTensor([len(x[2]) for x in batch])
+        max_y_len = y_lengths.max().item()
+        y_padded = torch.zeros(len(batch), max_y_len)
         for i in range(len(batch)):
-            wav_padded[i, :wav_lengths[i]] = batch[i][2]
+            y_padded[i, :y_lengths[i]] = batch[i][2]
 
-        return (
-            text_padded,
-            input_lengths,
-            spec_padded,
-            spec_lengths,
-            wav_padded,
-            wav_lengths,
-        )
+        return text_padded, input_lengths, spec_padded, spec_lengths, y_padded, y_lengths
 
 
-# -----------------------------
-# DistributedBucketSampler
-# -----------------------------
 import math
 import torch.distributed as dist
 from torch.utils.data.sampler import Sampler
@@ -135,11 +122,9 @@ class DistributedBucketSampler(Sampler):
         self.rank = rank
         self.shuffle = shuffle
 
-        # Assign each sample to a bucket based on spectrogram length
         self.buckets = [[] for _ in range(len(boundaries) + 1)]
-        for i in range(len(dataset)):
-            _, spec, _ = dataset[i]
-            length = spec.shape[1]
+        for i, (_, spec, _) in enumerate(dataset):
+            length = spec.size(1)
             idx_bucket = self._get_bucket(length)
             self.buckets[idx_bucket].append(i)
 
